9aa1d2b6eec62987421956ec3ad7ecf7e2501817
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-16 22:55:37 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + TypeScript + Vite
+
+Testing react with redux.
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>React + Redux</h1>
+      <p>
+        TBD.
+      </p>
+    </>
+  )
+}
+
+export default App
