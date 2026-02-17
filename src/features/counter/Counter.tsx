d29ddea53e6db16257c6653fde67e16cfd910c08
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-17 23:19:36 +1100

    Applied new button component to counter

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,3 +1,4 @@
+import Button from "../../components/Button.tsx";
 import { useAppDispatch, useAppSelector } from "../../hooks/hooks.ts";
 import { decrement, decrementByAmount, increment, incrementByAmount } from "./counterSlice.ts";
 
@@ -5,28 +6,34 @@ export default function Counter() {
   const count = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
 
+  const handleIncrement = () => () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => () => {
+    dispatch(decrement());
+  };
+
+  const handleIncrementByAmount = () => () => {
+    dispatch(incrementByAmount(10));
+  };
+
+  const handleDecrementByAmount = () => () => {
+    dispatch(decrementByAmount(10));
+  };
+
   return (
     <div>
       <p>
         Count: {count}
       </p>
       <p>
-        <button aria-label="Increment value" onClick={() => dispatch(increment())}>
-          Increment
-        </button>
-        &nbsp;
-        <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
-          Decrement
-        </button>
+        <Button onClick={handleIncrement()}>Increment</Button>
+        <Button onClick={handleDecrement()}>Decrement</Button>
       </p>
       <p>
-        <button aria-label="Increment value" onClick={() => dispatch(incrementByAmount(10))}>
-          Increment +10
-        </button>
-        &nbsp;
-        <button aria-label="Decrement value" onClick={() => dispatch(decrementByAmount(10))}>
-          Decrement -10
-        </button>
+        <Button onClick={handleIncrementByAmount()}>Increment +10</Button>
+        <Button onClick={handleDecrementByAmount()}>Decrement -10</Button>
       </p>
     </div>
   );
