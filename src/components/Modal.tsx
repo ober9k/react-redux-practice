39b3493bbf24c3c5d2781c1439b0b5a896039578
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-18 13:39:00 +1100

    Added basic modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,22 @@
+import type { ReactNode } from "react";
+import * as React from "react";
+import classes from "./Modal.module.css";
+
+type ModalProps = {
+  onClose: () => void,
+  children: ReactNode;
+}
+
+export default function Modal({ onClose, children }: ModalProps) {
+  return (
+    <>
+      <div className={classes.backdrop}></div>
+      <dialog open={true} className={classes.modal}>
+        <div className={classes.closeAction}>
+          <button onClick={onClose}>&times;</button>
+        </div>
+        {children}
+      </dialog>
+    </>
+  );
+}
