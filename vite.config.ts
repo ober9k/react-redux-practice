9d046d9cb38c8c887bc970d2a7ae237e6a58d0de
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-21 22:01:13 +1100

    Added missed path aliases to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
+import path from "path";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      /* path aliases to match `tsconfig.app.json` */
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@schemas": path.resolve(__dirname, "./src/schemas"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@types": path.resolve(__dirname, "././src/types")
+    },
+  },
   plugins: [
     react({
       babel: {
