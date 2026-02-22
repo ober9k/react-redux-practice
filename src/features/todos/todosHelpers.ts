784633d472f1ed8b8b530d022a1870a093149dd4
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-22 22:28:21 +1100

    Added types to mock todos data and some mock tags

--- src/features/todos/todosHelpers.ts
+++ src/features/todos/todosHelpers.ts
@@ -1,3 +1,4 @@
+import type { Tag } from "@types/Tag.ts";
 import type { Todo } from "@types/Todo.ts";
 
 export function getNextTodoId(state): number {
@@ -8,16 +9,32 @@ export function findTodoById(state, id: number): Todo {
   return state.todos.find((todo) => todo.id === id);
 }
 
-export const mockTodos = [{
+export const mockTags: Array<Tag> = [{
+  id: 1,
+  title: "Examples",
+},{
+  id: 2,
+  title: "Others",
+},{
+  id: 3,
+  title: "Miscellaneous",
+}];
+
+export const mockTodos: Array<Todo> = [{
   id: 1,
   title: "Example Todo #1",
   description: "Example Description",
-  tags: [],
+  tags: [
+    mockTags[1],
+    mockTags[2],
+  ],
   isCompleted: false,
 },{
   id: 2,
   title: "Example Todo #2",
   description: "Example Description",
-  tags: [],
+  tags: [
+    mockTags[1],
+  ],
   isCompleted: false,
 }];
