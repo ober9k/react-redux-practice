baf0f3c80165a5679f524afada84f2fdce2c3542
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-25 00:14:47 +1100

    Added basic logic for dealing with todo tags

--- src/features/todos/todosSlice.ts
+++ src/features/todos/todosSlice.ts
@@ -1,6 +1,7 @@
 import { findTodoById, getNextTodoId, mockTodos } from "@features/todos/todosHelpers.ts";
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "@store/store.ts";
+import type { Tag } from "@types/Tag.ts";
 import type { Todo } from "@types/Todo.ts";
 
 export interface TodosState {
@@ -43,10 +44,32 @@ export const todosSlice = createSlice({
       const { id } = action.payload;
       state.todos = state.todos.filter((todo) => todo.id !== id);
     },
+    /**
+     * TODO: this will be tidied up to possibly apply via `updateTodo` instead.
+     * TODO: simplify logic to remove double handling
+     * @param state
+     * @param action
+     */
+    toggleTodoTag: (state, action: PayloadAction<{ todo: Todo, tag: Tag }>) => {
+      const { id } = action.payload.todo;
+      const { tag } = action.payload;
+      const existingTodo = findTodoById(state, id);
+
+      if (existingTodo) {
+        const tagExists = existingTodo.tags.map((_) => _.id).includes(tag.id);
+
+        if (tagExists) {
+          existingTodo.tags = [...existingTodo.tags.filter((_) => _.id !== tag.id)];
+        }
+        else {
+          existingTodo.tags.push(tag);
+        }
+      }
+    },
   },
 });
 
-export const { addTodo, toggleTodo, deleteTodo } = todosSlice.actions;
+export const { addTodo, toggleTodo, deleteTodo, toggleTodoTag } = todosSlice.actions;
 
 export const selectTodos = (state: RootState) => state.todos.todos;
 
