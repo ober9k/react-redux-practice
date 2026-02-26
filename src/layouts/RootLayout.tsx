69c73759a1d93f82a39a90f0de53aa74dbe79d14
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-26 13:20:18 +1100

    Updated layout to extra header and narrow content body

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,12 +1,19 @@
 import Navigation from "@components/Navigation.tsx";
+import classes from "@layouts/RootLayout.module.css";
 import { Outlet } from "react-router";
 
 export default function RootLayout() {
   return (
     <>
-      <h1>React + Redux</h1>
-      <Navigation />
-      <Outlet />
+      <header className={classes.header}>
+        <h1>
+          React + Redux + Vite
+        </h1>
+        <Navigation />
+      </header>
+      <main className={classes.content}>
+        <Outlet />
+      </main>
     </>
   );
 }
